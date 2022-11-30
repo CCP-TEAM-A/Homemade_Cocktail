58c00f20b73187b7f3a6d624d77ae0739e44920d
조기범 <akfmzh1207@naver.com>
2022-11-30 17:05:52 +0900

    파이어베이스 연동중

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBqOA55QQLDZS0ZTuwV74qwan-RLnfpGSw",
+  authDomain: "cocktail-d4c7b.firebaseapp.com",
+  projectId: "cocktail-d4c7b",
+  storageBucket: "cocktail-d4c7b.appspot.com",
+  messagingSenderId: "653419923509",
+  appId: "1:653419923509:web:25dd69147c6d303613097c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
+
